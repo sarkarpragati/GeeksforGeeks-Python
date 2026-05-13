8c1417f764eada7b4ce69f41002e5b66e760c7c9
Pragati Sarkar <sarkarpragati2004@gmail.com>
2026-05-13 11:46:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/LinkedList Traversal/linked-list-traversal.py
@@ -0,0 +1,17 @@
+"""
+class Node:
+    def __init__(self, val):
+        self.data = val
+        self.next = None
+"""
+
+class Solution:
+    def printList(self, head):
+        curr = head
+        while curr:
+            
+            print(curr.data, end=" ")
+            curr = curr.next
+        
+        
+        
