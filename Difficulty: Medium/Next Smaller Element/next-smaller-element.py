dc341bcee761ebd673984998807245197d9472cc
Pragati Sarkar <sarkarpragati2004@gmail.com>
2026-05-14 16:23:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Next Smaller Element/next-smaller-element.py
@@ -0,0 +1,22 @@
+class Solution:
+	def nextSmallerEle(self, arr):
+	
+        n = len(arr)
+        
+        res = [-1] * n
+        stack = []
+        
+    
+        for i in range(n - 1, -1, -1):
+            
+            while stack and stack[-1] >= arr[i]:
+                stack.pop()
+            
+            
+            if stack:
+                res[i] = stack[-1]
+            
+            
+            stack.append(arr[i])
+            
+        return res
